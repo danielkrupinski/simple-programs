bdab59ff49803918139866a895c6ab43a2dbd543
danielkrupinski <danielkrupinski@outlook.com>
2018-01-16 16:43:43 +0100

    Upload file

---
+++ program4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	system("chcp 1250");
+	system("cls");
+	cout << "Podaj trzy liczby: ";
+	vector<int> numbers(3);
+	cin >> numbers[0] >> numbers[1] >> numbers[2];
+	sort(numbers.begin(), numbers.end());
+	cout << "Podane liczby w kolejności malejącej: " << endl;
+	cout << numbers[2] << endl;
+	cout << numbers[1] << endl;
+	cout << numbers[0] << endl;
+	cin.ignore();
+	getchar();
+	return 0;
+}
