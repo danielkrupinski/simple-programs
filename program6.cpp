1e2631d169c69008a340151d921ef0cbc1b3faa5
danielkrupinski <danielkrupinski@outlook.com>
2018-01-16 16:45:32 +0100

    Upload file

---
+++ program6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	system("chcp 1250");
+	system("cls");
+	cout << "Podaj liczbę: ";
+	int number;
+	cin >> number;
+	number % 2 == 0 ? cout << "Liczba jest parzysta." : cout << "Liczba jest nieparzysta.";
+	cin.ignore();
+	getchar();
+	return 0;
+}
