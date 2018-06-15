abfb71924804e02206742d478887950326a470d1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-15 18:00:27 +0200

    Update program5.cpp

--- program5.cpp
+++ program5.cpp
@@ -3,26 +3,17 @@
 
 using namespace std;
 
-void CheckNumber(const int& a)
-{
-    if (a != 0)
-    if (a > 0)
-    cout << "Liczba " << a << " jest dodatnia." << endl;
-    else cout << "Liczba " << a << " jest ujemna." << endl;
-    else cout << "Liczba " << a << " jest równa 0." << endl;
-}
-
 int main()
 {
-    system("chcp 1250");
-    system("cls");
     cout << "Podaj trzy liczby: ";
     vector<int> numbers(3);
     cin >> numbers[0] >> numbers[1] >> numbers[2];
-    CheckNumber(numbers[0]);
-    CheckNumber(numbers[1]);
-    CheckNumber(numbers[2]);
-    cin.ignore();
-    getchar();
+
+    for (auto& a : numbers)
+        if (a)
+            if (a > 0)
+                cout << "Liczba " << a << " jest dodatnia.\n";
+            else cout << "Liczba " << a << " jest ujemna.\n";
+        else cout << "Liczba " << a << " jest równa 0.\n";
     return 0;
 }
