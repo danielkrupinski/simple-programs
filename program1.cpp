7adc1706cbad41f7ef82b4f90346a01dee722dac
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-15 17:50:03 +0200

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -6,14 +6,9 @@ using namespace std;
 
 int main()
 {
-	system("chcp 1250");
-	system("cls");
 	cout << "Podaj trzy liczby: ";
 	vector<int> numbers(3);
 	cin >> numbers[0] >> numbers[1] >> numbers[2];
-	sort(numbers.begin(), numbers.end());
-	cout << "Największa jest liczba " << numbers[numbers.size()-1];
-	cin.ignore();
-	getchar();
+	cout << "Najwieksza jest liczba " << *max_element(numbers.begin(), numbers.end()) << ".\n";
 	return 0;
 }
