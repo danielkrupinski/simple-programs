c6684802de4e852993abddc64dd9cc8aa72d5ea8
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-15 17:53:46 +0200

    Update program3.cpp

--- program3.cpp
+++ program3.cpp
@@ -6,15 +6,11 @@ using namespace std;
 
 int main()
 {
-    system("chcp 1250");
-    system("cls");
     cout << "Podaj trzy liczby: ";
     vector<int> numbers(3);
     cin >> numbers[0] >> numbers[1] >> numbers[2];
     sort(numbers.begin(), numbers.end());
-    cout << "Podane liczby w kolejności rosnącej: " << endl;
-    cout << numbers[0] << '\n' << numbers[1] << '\n' << numbers[2];
-    cin.ignore();
-    getchar();
+    cout << "Podane liczby w kolejności rosnącej:\n";
+    cout << numbers[0] << '\n' << numbers[1] << '\n' << numbers[2] << '\n';
     return 0;
 }
